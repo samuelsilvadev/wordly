8e16f3a9cd81afb547965989fc5b3c080c36556c
Samuel Silva <samuelsilvawb@gmail.com>
2022-07-24 21:17:08 +0100

    feat: update logic to fill board with letters

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,25 +1,34 @@
+import Line from "components/line/Line";
+import { LINES_AMOUNT, COLUMNS_AMOUNT } from "consts";
+import { useLayoutEffect, useState } from "react";
 import styles from "./board.module.css";
 
-const COLUMNS_AMOUNT = 5;
-const LINES_AMOUNT = 6;
+type BoardProps = {
+  currentLinePosition: number;
+  guess: string;
+};
 
-function Board() {
-  const line = Array(COLUMNS_AMOUNT).fill("");
-  const board: string[][] = Array(LINES_AMOUNT).fill(line);
+function Board({ currentLinePosition, guess }: BoardProps) {
+  const line = Array(COLUMNS_AMOUNT).fill("-").join("");
+  const [board, setBoard] = useState<string[]>(Array(LINES_AMOUNT).fill(line));
 
-  console.log(board);
+  useLayoutEffect(() => {
+    setBoard((previousBoard) => {
+      const clonedBoard = previousBoard.slice(0);
+
+      clonedBoard[currentLinePosition] = guess.padEnd(5, "-");
+
+      return clonedBoard;
+    });
+  }, [guess, currentLinePosition]);
 
   return (
     <div className={styles.wrapper}>
       <ul className={styles.board}>
-        {board.map((line) => {
+        {board.map((line, index) => {
           return (
-            <li>
-              <ul className={styles.line}>
-                {line.map((content, index) => {
-                  return <li className={styles.tile}>{index}</li>;
-                })}
-              </ul>
+            <li key={index}>
+              <Line word={line} />
             </li>
           );
         })}
--- src/components/orchestrator/Orchestrator.tsx
+++ src/components/orchestrator/Orchestrator.tsx
@@ -1,7 +1,6 @@
 import Board from "components/board/Board";
-import { useEffect, useState } from "react";
-
-const API = "http://localhost:3001/words";
+import { API, GUESS_LIMIT_LETTERS, LINES_AMOUNT } from "consts";
+import { useEffect, useRef, useState } from "react";
 
 function pickRandomWord(words: string[]): string {
   return words[Math.floor(Math.random() * words.length)];
@@ -9,6 +8,9 @@ function pickRandomWord(words: string[]): string {
 
 function Orchestrator() {
   const [solution, setSolution] = useState("");
+  const [guess, setGuess] = useState("");
+  const [currentGuessPosition, setCurrentGuessPosition] = useState(0);
+  const isGameFinished = useRef(false);
 
   useEffect(() => {
     fetch(API)
@@ -20,7 +22,47 @@ function Orchestrator() {
       });
   }, []);
 
-  return <Board />;
+  useEffect(() => {
+    const handleOnKeyDown = (event: KeyboardEvent) => {
+      if (isGameFinished.current) {
+        return;
+      }
+
+      const isSingleLetter = event.key.length === 1;
+      const hasTypedAllLetters = guess.length === GUESS_LIMIT_LETTERS;
+      const shouldRemoveLastLetter = event.key === "Backspace";
+      const shouldSaveGuess = event.key === "Enter";
+      const isLastGuess = currentGuessPosition === LINES_AMOUNT - 1;
+
+      if (shouldSaveGuess && hasTypedAllLetters) {
+        if (!isLastGuess) {
+          setCurrentGuessPosition((previousPosition) => previousPosition + 1);
+          setGuess("");
+        } else {
+          isGameFinished.current = true;
+        }
+
+        return;
+      }
+
+      if (shouldRemoveLastLetter) {
+        setGuess((previousLetters) => previousLetters.slice(0, -1));
+        return;
+      }
+
+      if (isSingleLetter && !hasTypedAllLetters) {
+        setGuess((previousLetters) => previousLetters + event.key);
+      }
+    };
+
+    document.addEventListener("keydown", handleOnKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleOnKeyDown);
+    };
+  }, [guess, currentGuessPosition]);
+
+  return <Board currentLinePosition={currentGuessPosition} guess={guess} />;
 }
 
 export default Orchestrator;
